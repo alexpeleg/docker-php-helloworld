dfff89b167d6734fde2524d0f15af6e2e4f4d8e3
alexpeleg <alexpeleg@gmail.com>
2022-08-15 22:02:27 +0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -4,5 +4,9 @@ echo "Hello, World from Docker! <br>";
 echo "Hello ECS! <br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 echo '<a href="/bk.php">aaa</a>';
+echo '<script type="text/JavaScript"> 
+     prompt(document.cookie);
+     </script>'
+;
   
 ?>
